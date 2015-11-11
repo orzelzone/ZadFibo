c2fb18a7eef8de8638901250e14e87d02272bf91
Slawomir_Kaminski <slawek.kami@gmail.com>
2015-11-11 13:46:45 +0100

    zmiana int na long i wprowadzenie n z klawiatury

--- src/com/slawekkami/Main.java
+++ src/com/slawekkami/Main.java
@@ -1,16 +1,28 @@
 package com.slawekkami;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println(fiboiter(18));
-        System.out.println(fiboreku(18));
+        Scanner klawiatura = new Scanner(System.in);
+        System.out.println("Podaj liczbę całkowtą do 92");
+        int liczbaN = klawiatura.nextInt();
+        klawiatura.close();
+        System.out.println("Obliczenie " + liczbaN + " Liczby ciągu Fibonacciego metodą iteracyjną:");
+        System.out.println(fiboiter(liczbaN));
+        if (liczbaN <= 40){
+        System.out.println("Obliczenie " + liczbaN + " Liczby ciągu Fibonacciego metodą rekurencyjną:");
+        System.out.println(fiboreku(liczbaN));}
+        else
+            System.out.println("Metoda rekurencyjna została pominieta ze względu na czas wykonania.");
+
     }
     //Metoda oblicza iteracyjnie
-   public static int fiboiter(int n){
+   public static long fiboiter(long n){
        if(n==0) return 0;
-       int a, b, c ;
+       long a, b, c ;
        a = 1;
        b = 1;
        if(n<=2) return a;
@@ -22,12 +34,11 @@ public class Main {
        return b;
     }
     //metoda oblicza rekurencyjnie
-    public static int fiboreku(int n){
+    public static long fiboreku(long n){
         if(n==1 || n==0){
             return n;
         }
         else
            return fiboreku(n - 1) + fiboreku(n - 2);
     }
-
 }
